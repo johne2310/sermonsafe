23d39c9af4c8a735aa4b16e5a044e7f9eb172e99
John Evans <john@day41.com.au>
2018-07-12 14:14:44 +1000

    Update credentials.ts

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyABZEdzPdRWA4YPEvIFiKQfy86Lqluc3og",
-  authDomain: "sermonsafe-2661d.firebaseapp.com",
-  databaseURL: "https://sermonsafe-2661d.firebaseio.com",
-  projectId: "sermonsafe-2661d",
-  storageBucket: "sermonsafe-2661d.appspot.com",
-  messagingSenderId: "740217379597"
+
 };
